4eb9b166392edf83bd04b466e45de9072814b148
nattapon.r <nattapon.r@live.com>
2016-12-01 12:47:51 +0700

    cherry pick form stalk…

--- app/servers/push/handler/pushHandler.js
+++ app/servers/push/handler/pushHandler.js
@@ -29,38 +29,47 @@ handler.push = function (msg, session, next) {
 function pushMessage(app, session, body) {
     var onlineMembers = new Array();
     var offlineMembers = new Array();
-    //@ Try to push message to other ...
-    async.map(body.members, function (item, resultCallback) {
-        app.rpc.auth.authRemote.getOnlineUser(session, item, function (err, user) {
-            if (err || user === null) {
-                offlineMembers.push(item);
-            }
-            else {
-                onlineMembers.push(user);
-            }
-            resultCallback(null, item);
-        });
-    }, function (err, results) {
-        console.log("online %s: offline %s: room.members %s:", onlineMembers.length, offlineMembers.length, body.members.length);
-        //<!-- push chat data to other members in room.
+    //@ Try to push message to others.
+    if (body.members == "*") {
         var onPush = {
             route: Code_1.default.sharedEvents.ON_PUSH,
             data: { event: body.event, message: body.message }
         };
-        //<!-- Push new message to online users.
-        var uidsGroup = new Array();
-        async.map(onlineMembers, function iterator(val, cb) {
-            var group = {
-                uid: val.uid,
-                sid: val.serverId
+        channelService.broadcast("connector", onPush.route, onPush.data);
+    }
+    else if (body.members instanceof Array) {
+        async.map(body.members, function (item, resultCallback) {
+            app.rpc.auth.authRemote.getOnlineUser(session, item, function (err, user) {
+                if (err || user === null) {
+                    offlineMembers.push(item);
+                }
+                else {
+                    onlineMembers.push(user);
+                }
+                resultCallback(null, item);
+            });
+        }, function (err, results) {
+            console.log("online %s: offline %s: push.members %s:", onlineMembers.length, offlineMembers.length, body.members.length);
+            //<!-- push chat data to other members in room.
+            var onPush = {
+                route: Code_1.default.sharedEvents.ON_PUSH,
+                data: { event: body.event, message: body.message }
             };
-            uidsGroup.push(group);
-            cb(null, null);
-        }, function done() {
-            channelService.pushMessageByUids(onPush.route, onPush.data, uidsGroup);
-            //<!-- Push message to off line users via parse.
-            if (!!offlineMembers && offlineMembers.length > 0) {
-            }
+            //<!-- Push new message to online users.
+            var uidsGroup = new Array();
+            async.map(onlineMembers, function iterator(val, cb) {
+                var group = {
+                    uid: val.uid,
+                    sid: val.serverId
+                };
+                uidsGroup.push(group);
+                cb(null, null);
+            }, function done() {
+                channelService.pushMessageByUids(onPush.route, onPush.data, uidsGroup);
+                //<!-- Push message to off line users via parse.
+                if (!!offlineMembers && offlineMembers.length > 0) {
+                }
+            });
         });
-    });
+    }
 }
