751cea0bfbbf38392151e3a7a74c84c11ee20e64
nattapon.r <nattapon.r@live.com>
2016-12-13 14:45:40 +0700

    add master config.

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 "use strict";
-var devApi = "http://smelink.animation-genius.com:3002";
+var devApi = "http://git.animation-genius.com:3002";
+var masterApi = "http://smelink.animation-genius.com:3002";
 var config = {
     api: {
         authen: devApi + "/api/authenticate/verify"
@@ -20,7 +21,29 @@ var config = {
         secret: "ahoostudio_session_secret"
     }
 };
+var masterConfig = {
+    api: {
+        authen: masterApi + "/api/authenticate/verify"
+    },
+    chatDB: "mongodb://203.148.255.29:27017/chats",
+    fileDB: "",
+    port: 80,
+    timeout: 10000,
+    webserver: "http://git.animation-genius.com",
+    pushServer: "smelink.animation-genius.com",
+    ParseApplicationId: "newSMELink",
+    ParseRESTAPIKey: "link1234",
+    ParseMasterKey: "link1234",
+    pushPort: 4040,
+    pushPath: "/parse/push",
+    session: {
+        expire: "1 days",
+        secret: "ahoostudio_session_secret"
+    }
+};
 function getConfig() {
-    return config;
+    var conf = (process.env.NODE_ENV === "production") ? masterConfig : config;
+    console.log(process.env.NODE_ENV, conf.chatDB);
+    return conf;
 }
 exports.Config = getConfig();
--- config/config.ts
+++ config/config.ts
@@ -1,4 +1,5 @@
-﻿let devApi = "http://smelink.animation-genius.com:3002";
+﻿let devApi = "http://git.animation-genius.com:3002";
+let masterApi = "http://smelink.animation-genius.com:3002";
 
 const config = {
   api: {
@@ -22,8 +23,33 @@ const config = {
   }
 }
 
+const masterConfig = {
+  api: {
+    authen: `${masterApi}/api/authenticate/verify`
+  },
+  chatDB: "mongodb://203.148.255.29:27017/chats",
+  fileDB: "",
+  port: 80,
+  timeout: 10000,
+  webserver: "http://git.animation-genius.com",
+
+  pushServer: "smelink.animation-genius.com",
+  ParseApplicationId: "newSMELink",
+  ParseRESTAPIKey: "link1234",
+  ParseMasterKey: "link1234",
+  pushPort: 4040,
+  pushPath: "/parse/push",
+  session: {
+    expire: "1 days",
+    secret: "ahoostudio_session_secret"
+  }
+}
+
 function getConfig() {
-  return config;
+  let conf = (process.env.NODE_ENV === `production`) ? masterConfig : config;
+  console.log(process.env.NODE_ENV, conf.chatDB);
+
+  return conf;
 }
 
 export const Config = getConfig();
