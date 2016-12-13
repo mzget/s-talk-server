dbf1593e3f8dd6ecf4f5d622c39f9bc1535a641e
nattapon.r <nattapon.r@live.com>
2016-12-13 14:49:24 +0700

    production prepare.

--- app.ts
+++ app.ts
@@ -6,7 +6,7 @@ import { AccountService } from './app/services/accountService';
 import webConfig = require('./config/config');
 
 process.env.TZ = 'UTC';
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = 'production';
 process.on('uncaughtException', function (err) {
     console.error(' Caught exception: ' + err.stack);
 });
