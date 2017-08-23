8c6028a1ea1c8834de4be4e0e670288d3ae03fce
Nattapon Rattajak <rattajak.n@gmail.com>
2017-08-23 15:18:10 +0700

    no message

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ const hooks = new Map();
 const getHookApi = (appKey) => {
     return {
         apikey: "alcohol1234",
-        onPushByUids: "http://localhost:8999/api/message/pushByUids"
+        onPushByUids: "http://chitchats.ga:8999/api/message/pushByUids"
     };
 };
 const devConfig = {
