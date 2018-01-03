50b777a73fc0afa70bffc0e9bfbbdffa72660d7f
Nattapon Rattajak <rattajak.n@gmail.com>
2018-01-03 16:41:02 +0700

    add JC app-id.

--- config/config.ts
+++ config/config.ts
@@ -2,32 +2,36 @@
 let dev2Api = "http://203.150.95.146:9000";
 let masterApi = "http://203.148.250.152:3002";
 
-interface StalkHookApi {
+interface IStalkHookApi {
   apikey: string;
   onPushByUids: string;
 }
-const hooks = new Map<string, StalkHookApi>();
+const hooks = new Map<string, IStalkHookApi>();
 hooks.set("alcohol1234", {
   apikey: "alcohol1234",
-  onPushByUids: "https://chitchats.ga:8999/api/message/pushByUids"
+  onPushByUids: "https://chitchats.ga:8999/api/message/pushByUids",
 });
 hooks.set("survey1234", {
   apikey: "survey1234",
-  onPushByUids: "http://chitchats.ga:8998/api/message/pushByUids"
+  onPushByUids: "http://chitchats.ga:8998/api/message/pushByUids",
+});
+hooks.set("JC212224", {
+  apikey: "JC212224",
+  onPushByUids: "https://chitchats.ga:8999/api/message/pushByUids",
 });
 const getHookApi = (appKey: string) => {
   return hooks.get(appKey);
 };
 
 const devConfig = {
-  apiKeys: ["chitchat1234", "alcohol1234", "survey1234", "ooca1234"],
+  apiKeys: ["chitchat1234", "alcohol1234", "survey1234", "ooca1234", "JC212224"],
   api: {
     apikey: "chitchat1234",
     host: `${dev2Api}`,
     authen: `${dev2Api}/api/authenticate/verify`,
     chatroom: `${dev2Api}/api/chatroom`,
     chat: `${dev2Api}/api/stalk/chat`,
-    user: `${dev2Api}/api/stalk/user`
+    user: `${dev2Api}/api/stalk/user`,
   },
   timeout: 10000,
 
@@ -39,8 +43,8 @@ const devConfig = {
   pushPath: "/parse/push",
   session: {
     expire: "1 days",
-    secret: "ahoostudio_session_secret"
-  }
+    secret: "ahoostudio_session_secret",
+  },
 };
 
 function getConfig() {
@@ -51,7 +55,7 @@ function getConfig() {
 }
 
 export function getWebhook(appKey: string = "alcohol1234") {
-  let webhook = getHookApi(appKey);
+  const webhook = getHookApi(appKey);
   return webhook;
 }
 
