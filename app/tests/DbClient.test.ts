876402ded2fb5de79f4c362dfcf4860c0c1b64ac
Nattapon Rattajak <nattapon.r@live.com>
2018-03-25 06:07:30 +0700

    update db test.

--- app/tests/DbClient.test.ts
+++ app/tests/DbClient.test.ts
@@ -2,9 +2,8 @@ import { InitDatabaseConnection } from "../DbClient";
 import { DBConfig } from "../../config/config";
 
 test('db connection', () => {
-    InitDatabaseConnection(DBConfig.database_name).then(db => {
+    expect.assertions(1);
+    return InitDatabaseConnection(DBConfig.database_name).then(db => {
         expect(db.databaseName).toEqual("s-talk");
-    }).catch(err => {
-
     });
 });
